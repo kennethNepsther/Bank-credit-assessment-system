496795d08ec7a03620b8686c9f3a8387b9caa40e
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-04-14 02:43:40 +0100

    :lock: feat: securiity configurations

--- eurekaserver/src/main/java/it/nepstherti/eurekaserver/SecurityConfig.java
+++ eurekaserver/src/main/java/it/nepstherti/eurekaserver/SecurityConfig.java
@@ -1,2 +1,37 @@
-package it.nepstherti.eurekaserver;public class SecurityConfig {
+package it.nepstherti.eurekaserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(authorize -> authorize
+
+                        .anyRequest().authenticated()
+
+
+
+                )
+                //.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
+                //.build();
+
+
+
+        return http.build();
+    }
+
 }
